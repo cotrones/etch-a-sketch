e347affe0d419698b20f5e174ea71a48da296a52
cotrones <tomcotroneo@gmail.com>
2019-06-26 15:33:10 -0400

    add and style reset button with full functionality

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ let createGrid = function(num) {
 	
 	for (let i = 0; i < (num*num); i++) {
 		let gridBox = document.createElement("div");
-		let size = 800 / num;
+		let size = (800 - (num*2)) / num;
 
 		gridBox.className = "grid";
 		gridBox.style.height = size + "px";
@@ -18,6 +18,21 @@ let createGrid = function(num) {
 
 let sketch = function(event) {
 	event.target.classList.add('sketched');
+}
+
+let reset = function() {
+	let result = prompt("How many squares per side? (1-100)");
+
+	if (result < 1) {
+		reset();
+	} else if(result > 100) {
+		reset();
+	} else if(isNaN(result)) {
+		reset();
+	} else {
+		container.innerHTML = "";
+		createGrid(result);
+	}
 }
 
 createGrid(16);
